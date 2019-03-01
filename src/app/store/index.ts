7e5cc0fc91f506247d882b63a7f3fd94ad33fb56
anshul varshney <anshulvarshney10@gmail.com>
2019-02-28 23:13:25 -0500

    Add actions and reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -6,14 +6,14 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as whatsFlyingReducer from './whats-flying.reducer';
 
 export interface State {
-
+  whatsFlying: whatsFlyingReducer.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  whatsFlying: whatsFlyingReducer.reducer,
 };
-
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
--- src/app/store/whats-flying.actions.ts
+++ src/app/store/whats-flying.actions.ts
@@ -1,14 +1,27 @@
 import { Action } from '@ngrx/store';
 
 export enum WhatsFlyingActionTypes {
-  LoadWhatsFlyings = '[WhatsFlying] Load WhatsFlyings',
-  
-  
+  LoadWhatsFlying = '[WhatsFlying] Load WhatsFlying',
+  GetFlights = '[WhatsFlying] Get Flights',
+  GetFlightsSuccess = '[WhatsFlying] Get Flights Success',
+  GetFlightsFailure = '[WhatsFlying] Get Flights FAilure'
 }
 
-export class LoadWhatsFlyings implements Action {
-  readonly type = WhatsFlyingActionTypes.LoadWhatsFlyings;
+export class LoadWhatsFlying implements Action {
+  readonly type = WhatsFlyingActionTypes.LoadWhatsFlying;
 }
 
+export class GetFlights implements Action {
+  readonly type = WhatsFlyingActionTypes.GetFlights;
+}
 
-export type WhatsFlyingActions = LoadWhatsFlyings;
+export class GetFlightsSuccess implements Action {
+  readonly type = WhatsFlyingActionTypes.GetFlightsSuccess;
+  constructor(public payload: string) {}
+}
+
+export class GetFlightsFailure implements Action {
+  readonly type = WhatsFlyingActionTypes.GetFlightsFailure;
+}
+
+export type WhatsFlyingActions = LoadWhatsFlying | GetFlights | GetFlightsSuccess | GetFlightsFailure ;
